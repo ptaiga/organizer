a675847306b201eacdfa75f6d1a79b36d96bed35
ptaiga (pythonanywhere) <ptaiga@gmail.com>
2017-12-04 11:04:06 +0000

    Add ptaiga.pythonanywhere.com to allowed host

--- sorga/settings.py
+++ sorga/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x_em*=s2c7a+rsi!nqz^91fwnr59(0+17n2tmb_p(i1d#^^#5s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ptaiga.pythonanywhere.com']
 
 
 # Application definition
