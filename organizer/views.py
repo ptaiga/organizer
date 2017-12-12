d2080a002d19d6379ad557a06b51e77abdfc5f08
ptaiga (pythonanywhere) <ptaiga@gmail.com>
2017-12-12 13:40:48 +0000

    Update index for viewing 5 latest tasks

--- organizer/views.py
+++ organizer/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Task
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the organizer app index." )
+    latest_task_list = Task.objects.order_by('-pub_date')[:5]
+    output = ', '.join([t.task_name for t in latest_task_list])
+    return HttpResponse(output)
+    # return HttpResponse("Hello, world. You're at the organizer app index." )
 
 def project(request, project_id):
     return HttpResponse("You're looking at project %s." % project_id)
