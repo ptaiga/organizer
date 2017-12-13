c996d83ed07561fc218a7df5a67efa8e09fbc60c
ptaiga(win) <ptaiga@gmail.com>
2017-12-13 19:14:03 +0300

    Add 404 in project and task detailed views

--- organizer/views.py
+++ organizer/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
-from .models import Task
+from .models import Project, Task
 
 def index(request):
     latest_task_list = Task.objects.order_by('-pub_date')[:5]
@@ -11,7 +11,13 @@ def index(request):
     return render(request, 'organizer/index.html', context)
 
 def project(request, project_id):
-    return HttpResponse("You're looking at project %s." % project_id)
-
+    project = get_object_or_404(Project, pk=project_id)
+    return render(request, 'organizer/project.html', {'project': project})
+	
 def task(request, task_id):
-    return HttpResponse("You're looking at task %s." % task_id)
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    return render(request, 'organizer/task.html', {'task': task})
+	# return HttpResponse("You're looking at task %s." % task_id)
