7367691161365ca350d61c923b4e4bb469c3de37
ptaiga(win) <ptaiga@gmail.com>
2017-12-13 18:53:26 +0300

    Replace HttpResponse by render in index view

--- organizer/views.py
+++ organizer/views.py
@@ -1,18 +1,14 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Task
 
 def index(request):
     latest_task_list = Task.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('organizer/index.html')
     context = {
         'latest_task_list': latest_task_list,    
 	}
-    return HttpResponse(template.render(context, request))
-	# output = ', '.join([t.task_name for t in latest_task_list])
-    # return HttpResponse(output)
-    # return HttpResponse("Hello, world. You're at the organizer app index." )
+    return render(request, 'organizer/index.html', context)
 
 def project(request, project_id):
     return HttpResponse("You're looking at project %s." % project_id)
