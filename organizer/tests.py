a850aba5a1cbee3a4457242dceb7b1255cd2e494
ptaiga(win) <ptaiga@gmail.com>
2017-12-19 21:50:00 +0300

    Add first test for method was_published_resently in Task

--- organizer/tests.py
+++ organizer/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+import datetime
+from django.utils import timezone
+
+from .models import Task
+
+class TaskModelTests(TestCase):
+    def test_was_published_resently_with_future_task(self):
+        """
+        was_published_recently() return False for tasks whose pub_date is in the future
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_task = Task(pub_date=time)
+
+        self.assertIs(future_task.was_published_recently(), False)
+
