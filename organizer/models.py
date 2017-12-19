d6247df257c3149986854d0a157c682a80f649b4
ptaiga(win) <ptaiga@gmail.com>
2017-12-19 21:57:32 +0300

    Fix bug in was_published_recently method of Task model

--- organizer/models.py
+++ organizer/models.py
@@ -16,4 +16,5 @@ class Task(models.Model):
     def __str__(self):
 	    return self.task_name
     def was_published_recently(self):
-	    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
