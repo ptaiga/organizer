46e3d1585ede8e53e1465adcb2fa66e69981d397
ptaiga(win) <ptaiga@gmail.com>
2017-12-20 14:32:02 +0300

    Add two more tests for was_published_recently of Task

--- organizer/tests.py
+++ organizer/tests.py
@@ -15,3 +15,18 @@ class TaskModelTests(TestCase):
 
         self.assertIs(future_task.was_published_recently(), False)
 
+    def test_was_published_recently_with_old_task(self):
+        """
+        was_published_recently() returns False for tasks whose pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_task = Task(pub_date=time)
+        self.assertIs(old_task.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_task(self):
+        """
+        was_published_recently() returns True for tasks whose pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_task = Task(pub_date=time)
+        self.assertIs(recent_task.was_published_recently(), True)
