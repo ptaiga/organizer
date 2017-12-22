2df5149e7cd6c3345d10627d69a838a51892f4ce
ptaiga(win) <ptaiga@gmail.com>
2017-12-22 18:58:40 +0300

    Add basic customization in admin

--- organizer/admin.py
+++ organizer/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Project, Task
 
-admin.site.register(Project)
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 1
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['project_name']}),
+        ('Date information', {'fields': ['pub_date'],
+                                    'classes': ['collapse']}),
+    ]
+    inlines = [TaskInline]
+    list_display = ['project_name', 'pub_date']
+    list_filter = ['pub_date']
+    search_fields = ['project_name']
+
+class TaskAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['task_name']}),
+        ('Date information', {'fields': ['pub_date'],
+                                    'classes': ['collapse']}),
+    ]
+    list_display = ['task_name', 'pub_date', 'project', 'was_published_recently']
+    list_filter = ['pub_date']
+    search_fields = ['task_name']
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
