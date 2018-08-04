308856541eb0bf01219a21544b10d3eddcb7a384
ptaiga(win) <ptaiga@gmail.com>
2018-08-04 22:14:11 +0300

    Send simple email to user about today's tasks

--- sorga/cron.py
+++ sorga/cron.py
@@ -10,25 +10,41 @@ application = get_wsgi_application()
 
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
+from django.utils import timezone
 
 from sorga.private_settings import email_to, email_from, \
                                     email_auth_user, email_auth_password
 
-def task():
-    if send_mail(
-        '[Organizer] Today tasks',
-        "Your today tasks:",
-        email_from,
-        ['ptaiga@gmail.com'],
-        fail_silently=False,
-        auth_user=email_auth_user,
+from organizer.models import Task
+
+
+def send(email_to, subject, content):
+    if send_mail(subject, content, email_from, [email_to],
+        fail_silently=False, auth_user=email_auth_user,
         auth_password=email_auth_password
     ):
-        print("Email send")
-        return
+        print(f"Email to '{email_to}' send")
+        return True
     else:
-        print("Email doesn't send. Try again later")
-        return
+        print("Email to '{email_to}' doesn't send. Try again later")
+        return False
+
+
+def task():
+    for user in User.objects.all():
+        if not user.email: continue
+        tasks = Task.objects.filter(user=user,
+                                    done_flag=False,
+                                    due_date__date__lte=timezone.now())
+        if not tasks: continue
+        subject = f"[Organizer] {user}, your today's tasks"
+        content = f"{user} ({user.email}), your tasks for today:\n"
+        for task in tasks:
+            content += f" - {task.task_name} ({task.due_date.date()})\n"
+
+        send(user.email, subject, content)
+        print(content)
+
 
 if __name__ == "__main__":
     task()
