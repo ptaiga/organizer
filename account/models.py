93e05aa4f3082ccf1fca3dfef52392fc37735674
ptaiga(ubuntu) <ptaiga@gmail.com>
2020-04-13 07:11:45 +0300

    Add Account model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    num_rand_tasks = models.IntegerField(default=1)
