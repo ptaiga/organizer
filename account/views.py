51283ce1a9a601e4a2d1fad6b88b6f63df7cb90f
ptaiga(ubuntu) <ptaiga@gmail.com>
2020-04-15 07:13:15 +0300

    Update view and template for account model

--- account/views.py
+++ account/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'account/index.html')
+    user = request.user if request.user.is_authenticated else None
+    num_rand_tasks = user.account.num_rand_tasks #1
+    return render(request, 'account/index.html', {
+        'num_rand_tasks': num_rand_tasks
+        })
     # return HttpResponse(f"{request.user}, it's your account!")
