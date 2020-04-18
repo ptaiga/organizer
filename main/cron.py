6032e249c9fcde1ad0194072ac90146c8be48c8c
ptaiga(ubuntu) <ptaiga@gmail.com>
2020-04-19 00:07:30 +0300

    Update cron.py according to previous commit

--- main/cron.py
+++ main/cron.py
@@ -17,6 +17,7 @@ from main.private_settings import email_to, email_from, \
                                     email_auth_user, email_auth_password
 
 from organizer.models import Task
+from account.models import Account
 
 
 def send(email_to, subject, content):
@@ -57,10 +58,12 @@ def task():
                 content += f" - {task.task_name}"
                 content += f" ({task.due_date.date()})\n" if task.due_date else "\n"
 
-        random_tasks = Task.objects.filter(user=user,
-                                           done_flag=False,
-                                           project__done_flag=False,
-                                           due_date=None).order_by('?')[:1]
+        random_tasks = Task.objects.filter(
+            user=user,
+            done_flag=False,
+            project__done_flag=False,
+            due_date=None
+        ).order_by('?')[:Account.objects.get(user=user).num_rand_tasks]
         if random_tasks:
             content += f"\nRandom:\n"
             for task in random_tasks:
